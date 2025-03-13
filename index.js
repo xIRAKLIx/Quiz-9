3581e42798680e3f0aa6ef8631e73dd3d2fcc5bf
xIRAKLIx <iraklichelidze32@gmail.com>
2025-03-13 20:28:19 +0400

    s
    s

---
+++ index.js
@@ -0,0 +1,45 @@
+const itemInput = document.getElementById('item-input')
+const addItemBtn = document.getElementById('add-item-btn')
+const itemList = document.getElementById('item-list')
+
+
+
+function addItem() {
+    const itemText = itemInput.value.trim();
+    if (itemText === '') {
+      alert('Please enter a task!');
+      return;
+    }
+
+
+}
+    const taskItem = document.createElement('li');
+    taskItem.className = 'task-item';
+
+    const taskTextSpan = document.createElement('span');
+    taskTextSpan.textContent = itemText;
+    taskItem.appendChild(taskTextSpan);
+
+
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete-Item';
+    deleteBtn.addEventListener('click', () => {
+      taskList.removeChild(taskItem);
+    });
+    taskItem.appendChild(deleteBtn);
+
+
+    itemList.appendChild(taskItem);
+
+    itemInput.value = '';
+
+   
+
+
+addItemBtn.addEventListener('click', addItem)
+
+
+
+
+  
